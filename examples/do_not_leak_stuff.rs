cc37a7a3fa88ed5c1d936a42a03bf0aaabfb8dd3
tamasfe <me@tamasfe.dev>
2023-05-14 23:04:40 +0200

    initial commit

---
+++ examples/do_not_leak_stuff.rs
@@ -0,0 +1,77 @@
+use std::{
+    io::Cursor,
+    sync::Arc,
+    time::{Duration, Instant},
+};
+
+use polars::{
+    io::csv,
+    lazy::dsl::all,
+    prelude::{DataType, Field, IntoLazy, SerReader},
+};
+
+fn main() {
+    let seconds: u64 = std::env::args().nth(1).unwrap().parse().unwrap();
+
+    let asd = csv::CsvReader::new(Cursor::new(include_bytes!("../res/example1.csv")))
+        .with_dtypes(Some(Arc::new(
+            [
+                Field::new("foo_id", DataType::Utf8),
+                Field::new("bar_id", DataType::Utf8),
+                Field::new("stuff", DataType::UInt64),
+                Field::new("thing", DataType::UInt64),
+                Field::new("bar_stuff", DataType::UInt64),
+                Field::new("bar_value", DataType::UInt64),
+                Field::new("bar_thing", DataType::UInt64),
+                Field::new("bar_total", DataType::UInt64),
+                Field::new("capacity", DataType::UInt64),
+                Field::new("foo_capacity", DataType::UInt64),
+            ]
+            .into_iter()
+            .collect(),
+        )))
+        .has_header(true)
+        .finish()
+        .unwrap();
+
+    let bsd = csv::CsvReader::new(Cursor::new(include_bytes!("../res/example2.csv")))
+        .with_dtypes(Some(Arc::new(
+            [
+                Field::new("bar_id", DataType::Utf8),
+                Field::new("default_whatever", DataType::Float32),
+                Field::new("min_whatever", DataType::Float32),
+                Field::new("max_whatever", DataType::Float32),
+                Field::new("is_true", DataType::Boolean),
+            ]
+            .into_iter()
+            .collect(),
+        )))
+        .with_try_parse_dates(true)
+        .has_header(true)
+        .finish()
+        .unwrap();
+
+    let start = Instant::now();
+
+    loop {
+        let _asd = asd
+            .clone()
+            .lazy()
+            .groupby_stable(["timestamp"])
+            .agg([all().last()])
+            .collect()
+            .unwrap();
+
+        let _bsd = bsd
+            .clone()
+            .lazy()
+            .groupby_stable(["timestamp"])
+            .agg([all().last()])
+            .collect()
+            .unwrap();
+
+        if Instant::now().duration_since(start) > Duration::from_secs(seconds) {
+            break;
+        }
+    }
+}
